d6f3532357ac7eb256dfc0af09d385db2f10c9d6
hasan-hasanzade <hasanzade.web@gmail.com>
2022-06-10 13:48:42 +0400

    experience elave olundu

---
+++ src/components/Experience.jsx
@@ -0,0 +1,43 @@
+import React from 'react'
+
+const Experience = () => {
+  return (
+    <div className='experience'>
+       <div className="d-flex justify-content-center my-5">
+          <h1>Experience</h1>
+       </div>
+       <div className="container experience-wrapper">
+         <div className="timeline-block timeline-block-right">
+            <div className="marker"></div>
+            <div className="timeline-content">
+               <h3>2012-2013</h3>
+               <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Blanditiis ab consequatur, maxime placeat veritatis eius? Reprehenderit vel consectetur quibusdam animi, nobis voluptate. Neque tenetur perferendis, doloribus esse ut quo dolore.</p>
+            </div>
+         </div>
+         <div className="timeline-block timeline-block-left">
+         <div className="marker"></div>
+            <div className="timeline-content">
+               <h3>2012-2013</h3>
+               <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Blanditiis ab consequatur, maxime placeat veritatis eius? Reprehenderit vel consectetur quibusdam animi, nobis voluptate. Neque tenetur perferendis, doloribus esse ut quo dolore.</p>
+            </div>
+         </div>
+         <div className="timeline-block timeline-block-right">
+            <div className="marker"></div>
+            <div className="timeline-content">
+               <h3>2012-2013</h3>
+               <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Blanditiis ab consequatur, maxime placeat veritatis eius? Reprehenderit vel consectetur quibusdam animi, nobis voluptate. Neque tenetur perferendis, doloribus esse ut quo dolore.</p>
+            </div>
+         </div>
+         <div className="timeline-block timeline-block-left">
+         <div className="marker"></div>
+            <div className="timeline-content">
+               <h3>2012-2013</h3>
+               <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Blanditiis ab consequatur, maxime placeat veritatis eius? Reprehenderit vel consectetur quibusdam animi, nobis voluptate. Neque tenetur perferendis, doloribus esse ut quo dolore.</p>
+            </div>
+         </div>
+       </div>
+    </div>
+  )
+}
+
+export default Experience
